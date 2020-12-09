c2529b9a13bf638a3d793fac581720f9f0ba1e1e
Zack Wenthe <zack.wenthe@acquia.com>
2020-12-09 15:05:35 -0600

    s

--- df_custom_components/components/example_vue_unicorn_ss/example_vue_unicorn.js
+++ df_custom_components/components/example_vue_unicorn_ss/example_vue_unicorn.js
@@ -1,12 +1,13 @@
 var app = new Vue({
   el: '#example_vue_unicorn',
   data: {
-    name: window.uniname,
+    name: 'unicorn',
     mainColor: '#662d91',
     tailAccentColor: '#36c1d9',
     bodyColor: '#efe2B4',
     footColor: '#05f1CD',
     wingColor: '#fff',
     hornColor: '#c69'
-  }
+  },
+
 })
